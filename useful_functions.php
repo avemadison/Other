6282cf0ad217e315483d4d1b7c2119f8d65dad1f
Sofi <avemadison@rambler.ru>
2016-10-31 23:54:15 +0200

    Update

--- useful_functions.php
+++ useful_functions.php
@@ -12,3 +12,71 @@ function fix_name(&$n1, &$n2, &$n3)
     $n3 = ucfirst(strtolower($n3));
     return $n1. ' '. $n2. ' '. $n3.' ';
 }
+
+/*
+* array in <pre>
+*/
+function dd($a){
+    echo "<pre>";
+    print_r($a);
+    echo "</pre>";
+}
+/*
+* @return bool 
+*/
+function requestIsPost()
+{
+    return strtolower($_SERVER['REQUEST_METHOD']) == 'post';
+}
+/**
+* @return string|null
+*/
+function post($key, $default = null)
+{
+    return isset($_POST[$key]) ? $_POST[$key] : $default;
+}
+/**
+* @return string|null
+*/
+function get($key, $default = null)
+{
+    
+    return isset($_GET[$key]) ? $_GET[$key] : $default;
+}
+
+function requestIsGet()
+{
+    return (bool)$_GET;
+}
+
+/**
+* @return bool
+*/
+function formIsValid()
+{
+	return post('username') != '' 
+		   && post('email') != '' 
+		   && post('message') != '';
+}
+/*
+* redirect
+*/
+function redirect($to)
+{
+    header('Location: ' . $to);
+    die;
+}
+
+function saveInFile (array $arr, $filename)
+{
+    $comment = serialize($arr);
+    $res = file_put_contents(
+        $filename,
+        $comment . PHP_EOL,
+        FILE_APPEND
+    );
+
+    if (!$res) {
+        die('Error');
+    }
+}
